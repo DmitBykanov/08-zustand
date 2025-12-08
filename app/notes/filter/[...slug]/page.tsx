5fa04b0338265ed74575422a6db405ea894b672f
dmitbykanov <dmitbykanov@gmail.com>
2025-12-08 14:36:50 +0200

    add metadata

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -2,11 +2,24 @@ import { QueryClient, dehydrate } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
 import { HydrationBoundary } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
 
 export interface NotesPageProps {
   params: Promise<{ slug: string[] }>;
+}
+
+export async function generateMetadata({
+  params,
+}: NotesPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const tag = slug?.[0] ?? "all";
+
+  return {
+    title: `Notes - ${tag.charAt(0).toUpperCase() + tag.slice(1)}`,
+    description: `A collection of notes tagged with "${tag}".`,
+  };
 }
 
 export default async function NotesPage({ params }: NotesPageProps) {
